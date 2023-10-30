4226fc95c71c954c1e129b11ea86b1de8fa3adf3
Abdanfikri <abdanfikri21@gmail.com>
2023-10-30 09:50:25 +0800

    pembaruan

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,21 @@
 public class Main {
+
+    public static int Faktorial(int angka) {
+        if (angka < 0) {
+            throw new IllegalArgumentException("Angka harus non-negatif.");
+        } else if (angka == 0 || angka == 1) {
+            return 1;
+        } else {
+            int x = 1;
+            for (int i = angka; i > 0; i--) {
+                x *= i;
+            }
+            return x;
+        }
+
+    }
+
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Hello world!");
