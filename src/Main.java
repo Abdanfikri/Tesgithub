06805791f48fdce12838f5b3b613b5a650535913
Abdanfikri <abdanfikri21@gmail.com>
2023-10-30 10:00:27 +0800

    pembaruan

--- src/Main.java
+++ src/Main.java
@@ -27,5 +27,10 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Hello world!");
 
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+
+
     }
 }
